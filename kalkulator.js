124115ff274a163c942c36d8719c4eb8a7b2f2d7
Mukhlis_Dev <39901695+mukhlis123@users.noreply.github.com>
2019-05-22 22:17:54 +0700

    Kalkulator

---
+++ kalkulator.js
@@ -0,0 +1,26 @@
+var tombol = document.querySelector(".container-tombol");
+var layar = document.querySelector("#layar");
+
+tombol.addEventListener("click", function(e){
+    
+    var tombolClick = e.target;
+    var nilaiTombol = tombolClick.innerText;
+
+    //"ini adalah string" + ini variable + "string lagi"
+    //"ini adalah string" + ini function()
+    //variable + ini function()
+
+    if(nilaiTombol == "C"){
+        layar.value = "";
+    }
+    else if(nilaiTombol == "<"){
+        layar.value = layar.value.slice(0,-1);
+    }
+    else if(nilaiTombol == "="){
+        layar.value = eval(layar.value);
+    }
+    else{
+        layar.value = layar.value + nilaiTombol;
+    }
+});
+    
